80d36784f34140af4ba3ba761e27dc348226fde7
GMTUDS <142052001+GMTUDS@users.noreply.github.com>
2024-05-28 09:37:16 -0300

    vn+2

--- ulpWeb1.io/script.js
+++ ulpWeb1.io/script.js
@@ -50,7 +50,7 @@ function imagenSiguiente() {
     }
 }
 
-cargarImagenes();
+
 
 function avanzaAuto(){
     setInterval( function(){
@@ -61,3 +61,73 @@ function avanzaAuto(){
 
 
 avanzaAuto();
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var emerg = document.getElementById("vnt");
+    var btnCerrar = document.querySelector(".btn_cerrar");
+    var formContacto = document.getElementById("frm_contacto");
+    var respuesta = document.getElementById("respuesta");
+    var botonContacto = document.getElementById("boton_contacto");
+
+
+    botonContacto.onclick = function () {
+        emerg.style.display= "block";
+    };
+
+
+    btnCerrar.onclick= function(){
+        emerg.style.display = "none";
+    }
+
+    window.onclick = function (event){
+        if(event.target == emerg){
+            emerg.style.display= "none";
+        }
+
+
+    };
+
+    formContacto.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        var nombre = document.getElementById("nombre");
+        var apellido = document.getElementById("apellido");
+        var telefono = document.getElementById("telefono");
+        var email = document.getElementById("email");
+
+        if (nombre.value === "" || apellido.value === "" || telefono.value === "" || email.value === ""){
+            respuesta.innerHTML= "Campos obligatorios incompletos";
+            return;
+        }
+
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email.value)){
+            respuesta.innerHTML = "Email invalido";
+            return;
+        }
+
+        var telefonoRegex= /^\d{8,12}$/;
+        if(!telefonoRegex.test(telefono.value)){
+            respuesta.innerHTML = "Número de telefono Invalido - Ingrese un número valido";
+            return;
+        }
+
+
+
+        respuesta.innerHTML = "";
+        var datosEnviados = document.createElement("p");
+        datosEnviados.innerHTML = "Nombre: "+ nombre.value + "<br>" + "Apellido: " + apellido.value + "<br>" + "Telefono: " + telefono.value + "<br>" + "Correo: " + email.value;
+        respuesta.appendChild(datosEnviados);
+
+
+        nombre.value = "";
+        apellido.value = "";
+        telefono.value = "";
+        email.value = "";
+
+    });
+    cargarImagenes();
+});
+
+
